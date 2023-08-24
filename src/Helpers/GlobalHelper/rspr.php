fdc532dfc8ab530b7ae7d6b7c4be60f951193f04
Dan <work.dcisneros@gmail.com>
2023-08-24 20:57:03 +0800

    [UPDATE] v10 - rspr global helper

--- src/Helpers/GlobalHelper/rspr.php
+++ src/Helpers/GlobalHelper/rspr.php
@@ -1,8 +1,10 @@
 <?php
 
 use RSPR\LaravelStarter\Traits\PublicFileImportTrait;
+use RSPR\LaravelStarter\Traits\RouteTrait;
 
 class rspr
 {
     use PublicFileImportTrait;
+    use RouteTrait;
 }
---
+++ src/Traits/RouteTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace RSPR\LaravelStarter\Traits;
+
+trait RouteTrait
+{
+    /**
+     * Check current route name
+     *
+     * @param string $routeName - the route name to be check
+     * @param string $returnStringIfTrue - the return string if true
+     * @return string [$name, 'active', '']
+     */
+    public function isRoute(null|string $routeName = null, string $returnStringIfTrue = 'active'): bool
+    {
+        $rtn = '';
+        $name = \Request::route()->action['as'];
+
+        if (is_null($routeName)) {
+            $rtn = $name;
+        } else {
+            if (!empty($name)) {
+                if (is_array($routeName)) {
+                    if (in_array($name, $routeName)) {
+                        $rtn = $returnStringIfTrue;
+                    }
+                } elseif ($name == $routeName) {
+                    $rtn = $returnStringIfTrue;
+                }
+            }
+        }
+
+        return $rtn;
+    }
+}
