e39cc80b7345d26f7e3d79dbbadd6009f74f5e10
Dan <c-daniel@chronostep.com>
2024-04-19 10:09:07 +0800

    [UPDATE] v11 - path error on publish

--- src/Traits/ServiceProviderTrait.php
+++ src/Traits/ServiceProviderTrait.php
@@ -162,7 +162,7 @@ trait ServiceProviderTrait
         $this->pubModel = [
             $this->customCurrentPath('app/Models/User.php.tmp') => $this->customProjectPath('app/Models/User.php.tmp'),
             $this->customCurrentPath('app/Traits/Model/ModelTrait.php.txt') => $this->customProjectPath('app/Traits/Model/ModelTrait.php'),
-            $this->customCurrentPath('app/Traits/ModelCustomSoftDeletesTrait.php.txt') => $this->customProjectPath('app/Traits/ModelCustomSoftDeletesTrait.php'),
+            $this->customCurrentPath('app/Traits/Model/ModelCustomSoftDeletesTrait.php.txt') => $this->customProjectPath('app/Traits/Model/ModelCustomSoftDeletesTrait.php'),
             $this->customCurrentPath('app/Models/Model.php.txt') => $this->customProjectPath('app/Models/Model.php'),
             $this->customCurrentPath('app/Models/ModelAuthenticatable.php.txt') => $this->customProjectPath('app/Models/ModelAuthenticatable.php'),
             // $this->customCurrentPath('app/Models/ModelCompoships.php.txt') => $this->customProjectPath('app/Models/ModelCompoships.php')
